ed4a9e74b6d473d35eeff9e0e4aeadc1d828110c
frankdb <fd.barros@yahoo.com>
2019-03-12 09:23:28 -0700

    Init commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 // import files and packages up here
+const express = require("express");
+const data = require("./data.json");
 
+const morgan = require("morgan");
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+// app.use();
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/data", (req, res) => {
+  res.json(data);
+});
 
 // finally export the express application
 module.exports = app;
